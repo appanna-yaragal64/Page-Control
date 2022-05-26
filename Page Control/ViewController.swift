42bc5dd170125024e4897540592c3a726a963244
Appanna Yaragal <appanna@innocirc.com>
2022-05-26 17:11:16 +0530

    Initial Commit

---
+++ Page Control/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Page Control
+//
+//  Created by Appanna Yaragal on 26/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
